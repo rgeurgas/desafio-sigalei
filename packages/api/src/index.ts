11d5831a1fb01356e6d24d31dca76f2513513ba3
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2020-07-27 22:44:45 -0300

    add router

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world');
+import Koa = require('koa');
+// eslint-disable-next-line import/first
+import { router } from './router';
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  ctx.body = 'hello world';
+  return next();
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(8000, () => {
+  // eslint-disable-next-line no-console
+  console.log('server is running');
+});
