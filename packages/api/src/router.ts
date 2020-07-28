ffdd5a501bda783c9cd9ed8ff9159fb29f3472ee
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2020-07-27 23:37:48 -0300

    add schema

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,3 +1,7 @@
+import { execute } from 'graphql-api-koa';
+import { schema } from './graphql';
+
+// eslint-disable-next-line import/order
 import Router = require('@koa/router');
 
 const router = new Router();
@@ -5,5 +9,7 @@ const router = new Router();
 router.get('/hello', (ctx) => {
   ctx.body = 'hello';
 });
+
+router.post('/graphql', execute({ schema }));
 
 export { router };
